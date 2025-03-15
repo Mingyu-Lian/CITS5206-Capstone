93977af237afa922e211caa0ec0002ceb00af297
YuanHe <heyuan0888@gmail.com>
2025-03-15 21:58:20 +0800

    add code for mongodb connection

--- server/backend/server.js
+++ server/backend/server.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
+const User = require("./models/User")
+const mongoose = require("mongoose"); 
 
 dotenv.config();
-//connectDB();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongodb:27017/test";
+console.log(MONGO_URI);
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, Docker with MongoDB!");
+});
+app.listen(5000, () => console.log(`Server running on port 5000`));
+
+
+
+  
+ 
 
 //app.use("/api/auth", authRoutes);
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
